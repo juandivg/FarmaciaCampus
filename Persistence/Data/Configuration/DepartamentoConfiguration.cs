9ae02fedb5e8c720eba55b3338edc992961d7a98
jeangalvis <campusjean2023@gmail.com>
2023-09-22 16:00:48 -0500

    feat(Persistence): :sparkles: dbset

---
+++ Persistence/Data/Configuration/DepartamentoConfiguration.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Data.Configuration
+{
+    public class DepartamentoConfiguration : IEntityTypeConfiguration<Departamento>
+    {
+        public void Configure(EntityTypeBuilder<Departamento> builder)
+        {
+            builder.ToTable("Departamento");
+            builder.Property(p=>p.Nombre).HasColumnName("NombreDepartamento").HasMaxLength(250).IsRequired();
+            builder.HasOne(p=>p.Pais).WithMany(p=>p.Departamentos).HasForeignKey(p=>p.IdPaisfk);
+        }
+    }
+}
--- Persistence/FarmaciaCampusContext.cs
+++ Persistence/FarmaciaCampusContext.cs
@@ -22,4 +22,32 @@ public class FarmaciaCampusContext : DbContext
         .WithOne(p=>p.Receta)
         .HasForeignKey<Venta>(p=>p.IdRecetafk);
     }
+    public DbSet<Barrio> Barrios { get; set; }
+    public DbSet<Cargo> Cargos { get; set; }
+    public DbSet<Ciudad> Ciudades { get; set; }
+    public DbSet<Compra> Compras { get; set; }
+    public DbSet<Departamento> Departamentos { get; set; }
+    public DbSet<DireccionEmpleado> DireccionEmpleados { get; set; }
+    public DbSet<DireccionPaciente> DireccionPacientes { get; set; }
+    public DbSet<DireccionProveedor> DireccionProveedores { get; set; }
+    public DbSet<Empleado> Empleados { get; set; }
+    public DbSet<Paciente> Pacientes { get; set; }
+    public DbSet<Pais> Paises { get; set; }
+    public DbSet<Producto> Productos { get; set; }
+    public DbSet<ProductoCompra> ProductoCompras { get; set; }
+    public DbSet<ProductoReceta> ProductoRecetas { get; set; }
+    public DbSet<ProductoVenta> ProductoVentas { get; set; }
+    public DbSet<Proveedor> Proveedores { get; set; }
+    public DbSet<ProveedorProducto> ProveedorProductos { get; set; }
+    public DbSet<Receta> Recetas { get; set; }
+    public DbSet<Rol> Roles { get; set; }
+    public DbSet<TelefonoEmpleado> TelefonoEmpleados { get; set; }
+    public DbSet<TelefonoPaciente> TelefonoPacientes { get; set; }
+    public DbSet<TelefonoProveedor> TelefonoProveedores { get; set; }
+    public DbSet<TipoProducto> TipoProductos { get; set; }
+    public DbSet<TipoTelefono> TipoTelefonos { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
+    public DbSet<Venta> Ventas { get; set; }
+
+
 }
